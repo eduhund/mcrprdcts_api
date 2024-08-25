d32937ffd6318f7d64ba6615bd68880bc9ad5ffe
Roman Nebel <im.roman.nebel@gmail.com>
2024-08-25 18:48:09 +0200

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import api from "./api/index.js";
 import webhook from "./webhook/index.js";
@@ -9,6 +10,7 @@ import { startDatabase } from "./services/mongo/mongo.js";
 const { PORT = 3000 } = process.env;
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api", api);
